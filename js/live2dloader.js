9969fa929ad64d15411ec75221e6ae1bb62c3c5b
KJ <65823167+KJook@users.noreply.github.com>
2022-09-29 20:36:28 +0800

    Create live2dloader.js

---
+++ js/live2dloader.js
@@ -0,0 +1,78 @@
+$(function() {
+	//Make the DIV element draggagle:
+	$("#live2dc")
+		.click(function() {
+			$.message({
+				message: "欢迎访问KJ Blog",
+				title: "你好鸭",
+				type: "info",
+				autoHide: !1,
+				time: "3000"
+			})
+		});
+	dragElement(document.getElementById(("live2dc")));
+
+	function dragElement(elmnt) {
+		var pos1 = 0,
+			pos2 = 0,
+			pos3 = 0,
+			pos4 = 0;
+		if (document.getElementById(elmnt.id + "header")) {
+			/* if present, the header is where you move the DIV from:*/
+			document.getElementById(elmnt.id + "header")
+				.onmousedown = dragMouseDown;
+		} else {
+			/* otherwise, move the DIV from anywhere inside the DIV:*/
+			elmnt.onmousedown = dragMouseDown;
+		}
+
+		function dragMouseDown(e) {
+			e = e || window.event;
+			// get the mouse cursor position at startup:
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			document.onmouseup = closeDragElement;
+			// call a function whenever the cursor moves:
+			document.onmousemove = elementDrag;
+		}
+
+		function elementDrag(e) {
+			e = e || window.event;
+			// calculate the new cursor position:
+			pos1 = pos3 - e.clientX;
+			pos2 = pos4 - e.clientY;
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			// set the element's new position:
+			elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+			elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		}
+
+		function closeDragElement() {
+			/* stop moving when mouse button is released:*/
+			document.onmouseup = null;
+			document.onmousemove = null;
+		}
+	}
+
+	if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		$("#live2dc")
+			.hide();
+		// 执行相应代码或直接跳转到手机页面
+	} else {
+		var resourcesPath = '/';
+		var backImageName = '';
+		var modelDir = ['Hiyori'];
+		initDefine(resourcesPath, backImageName, modelDir);
+	}
+	if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		$("#live2dc")
+			.hide();
+		// 执行相应代码或直接跳转到手机页面
+	} else {
+		var resourcesPath = '/';
+		var backImageName = '';
+		var modelDir = ['Hiyori'];
+		initDefine(resourcesPath, backImageName, modelDir);
+	}
+});
