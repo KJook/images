952cd659415a7dce20ca8a9ebf9b061d37835b92
KJ <65823167+KJook@users.noreply.github.com>
2022-09-30 17:57:29 +0800

    Update live2dloaderpure4.js

--- js/live2dloaderpure4.js
+++ js/live2dloaderpure4.js
@@ -44,11 +44,13 @@ window.onload = function () {
 			document.onmousemove = null;
 		}
 	}
-
+	
 	if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+		console.log("hide Live2D");
 		$("#live2dc").hide();
 		// 执行相应代码或直接跳转到手机页面
 	} else {
+		console.log("init Live2D");
 		var resourcesPath = 'https://i.kjqaq.com/live2d/';
 		var backImageName = '';
 		var modelDir = ['Hiyori'];
