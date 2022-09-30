9214dbe74681d73001e0603c5deed398e77bde66
KJ <65823167+KJook@users.noreply.github.com>
2022-09-30 14:02:55 +0800

    Update live2dloaderpure.js

--- js/live2dloaderpure.js
+++ js/live2dloaderpure.js
@@ -49,7 +49,7 @@
 			.hide();
 		// 执行相应代码或直接跳转到手机页面
 	} else {
-		var resourcesPath = '/';
+		var resourcesPath = 'https://i.kjqaq.com/live2d/';
 		var backImageName = '';
 		var modelDir = ['Hiyori'];
 		initDefine(resourcesPath, backImageName, modelDir);
